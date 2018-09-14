89d55f5b0ff2f4a4a26a706be0db413b6199f9f1
quiquek007 <quiquek007@gmail.com>
2018-09-14 15:48:07 +0300

    Solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,38 @@
 module.exports = function solveEquation(equation) {
-  // your implementation
+  let A = "",
+    B = "",
+    C = "",
+    D = 0,
+    x1,
+    x2,
+    pos;
+  //for A
+  for(let i=0;i<equation.length;i++) {
+    if (equation[i] == '*') {
+      pos = i;
+      break;
+    }
+    else if (equation[i] == ' ') continue;
+    else A+=equation[i];
+  }
+  for(let i=pos+5;i<equation.length;i++) {
+    if (equation[i] == '*') break;
+    else if (equation[i] == ' ') continue;
+    else B+=equation[i];
+  }
+  for(let i=equation.length-1;i!=0;i--) {
+    if (equation[i] == 'x') break;
+    else if (equation[i] == ' ') continue;
+    else C=equation[i] + C;
+  }
+  A = Number(A);
+  B = Number(B);
+  C = Number(C);
+  //D=B2-4*A*C
+  D = B*B - 4*A*C;
+  // x1=(-В+D1/2)/2А .
+  x1 = (-B + Math.sqrt(D)) / (2*A);
+  // x2=(-В-D1/2)/2А.
+  x2 = (-B - Math.sqrt(D)) / (2*A);
+  return x1<x2 ? [Math.round(x1),Math.round(x2)] : [Math.round(x2),Math.round(x1)];
 }
